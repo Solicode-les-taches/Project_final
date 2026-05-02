6dbd216751588a5044e5a485ae4973425aee4483
OUSSAMA <oussamacharef71@gmail.com>
2026-05-02 19:28:03 +0100

    Set up home page layout

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+    <title>Tangier Vibes - Home</title>
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+    <?php require 'includes/header.php'; ?>
+
+
+
+
+    <script src="assets/js/main.js"></script>
+    
+</body>
+</html>
+
+    
+
