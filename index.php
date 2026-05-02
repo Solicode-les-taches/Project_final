a7a4f437e6ea84aef86ef6db9de05008c3c2f0ad
OUSSAMA <oussamacharef71@gmail.com>
2026-05-02 20:16:19 +0100

    Include footer assets on home page

--- index.php
+++ index.php
@@ -10,16 +10,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
     <title>Tangier Vibes - Home</title>
-    <link rel="stylesheet" href="assets/css/header.css">
     <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <body>
 
     <?php require 'includes/header.php'; ?>
-
-
-
-
+    
+    
+    
+    
+    <?php require 'includes/footer.php'; ?>
     <script src="assets/js/main.js"></script>
     
 </body>
