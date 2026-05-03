750b385e8cf33a23423b0f92ad78d07820f5493a
OUSSAMA <oussamacharef71@gmail.com>
2026-05-03 23:01:39 +0100

    Add hero section to home page

--- index.php
+++ index.php
@@ -8,17 +8,52 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
     <title>Tangier Vibes - Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+    
     <link rel="stylesheet" href="assets/css/main.css">
     <link rel="stylesheet" href="assets/css/header.css">
     <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="assets/css/home.css">
 </head>
 <body>
 
     <?php require 'includes/header.php'; ?>
     
-    
+     <section class="hero_section">
+
+        <img src="assets/img/home.jpg" alt="Tangier - backgrond image hero" class="hero_bg_img" loading="lazy">
+
+        <div class="hero_overlay"></div>
+
+        <div class="hero_content">
+
+            <p class="hero_label">WELCOME TO YOUR GATEWAY TO AFRICA</p>
+
+            <h1 class="hero_title">
+                Experience the Magic<br>
+                of <span class="hero_highlight">Tangier</span>
+            </h1>
+
+            <p class="hero_desc">
+                Discover hidden beaches, legendary cafes, exquisite<br>
+                restaurants, and historic landmarks in the Pearl of the North.
+            </p>
+
+            <div class="hero_btns">
+                <a href="explore.php" class="hero_btn_primary">
+                    Start Exploring
+                </a>
+
+                <a href="#" class="hero_btn_outline">
+                    Top Picks <i class="fa-solid fa-arrow-right"></i>
+                </a>
+            </div>
+
+        </div>
+
+    </section>
+
     
     
     <?php require 'includes/footer.php'; ?>
