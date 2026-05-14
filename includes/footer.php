99251acdeacc751819ce076478bd4824db9a2614
OUSSAMA <oussamacharef71@gmail.com>
2026-05-14 11:51:51 +0200

    Update footer markup

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
-<!-- Footer Section -->
+
 <footer class="site_footer">
     <div class="footer_top">
         
-        <!-- About Column -->
+        <!-- Brand Section -->
         <div class="footer_about">
             <a href="index.php" class="logo footer_logo">
                 <div class="logo_icon"><i class="fa-solid fa-compass"></i></div>
@@ -14,51 +14,47 @@
             </p>
         </div>
 
-        <!-- Quick Links Column -->
+        <!-- Navigation Columns -->
         <div class="footer_col">
             <h4 class="footer_col_title">Quick Links</h4>
             <ul class="footer_links">
-                <li><a href="index.php" class="footer_link">Home</a></li>
+                <li><a href="../index.php" class="footer_link">Home</a></li>
                 <li><a href="#" class="footer_link">Top Places</a></li>
-                <li><a href="explore.php" class="footer_link">Explore</a></li>
-                <li><a href="#" class="footer_link">Favorites</a></li>
+                <li><a href="../pages/explore.php" class="footer_link">Explore</a></li>
             </ul>
         </div>
    
-        <!-- Categories Column -->
         <div class="footer_col">
             <h4 class="footer_col_title">Categories</h4>
             <ul class="footer_links">
-                <li><a href="#" class="footer_link">Hotels</a></li>
                 <li><a href="#" class="footer_link">Restaurants</a></li>
-                <li><a href="#" class="footer_link">Cafes</a></li>
                 <li><a href="#" class="footer_link">Beaches</a></li>
-                <li><a href="#" class="footer_link">Parks</a></li>
-                <li><a href="#" class="footer_link">Museum</a></li>
+                <li><a href="#" class="footer_link">Museums</a></li>
             </ul>
         </div>
 
-        <!-- Social Media Column -->
+        <!-- Social Connect -->
         <div class="footer_col">
             <h4 class="footer_col_title">Follow Us</h4>
             <div class="footer_socials">
-                <a href="#" class="social_icon" aria-label="Facebook">
+                <a href="#" class="social_icon" title="Facebook">
                     <i class="fa-brands fa-facebook-f"></i>
                 </a>
-                <a href="#" class="social_icon" aria-label="Instagram">
+                <a href="#" class="social_icon" title="Instagram">
                     <i class="fa-brands fa-instagram"></i>
                 </a>
-                <a href="#" class="social_icon" aria-label="Twitter">
+                <a href="#" class="social_icon" title="X (Twitter)">
                     <i class="fa-brands fa-x-twitter"></i>
                 </a>
             </div>
         </div>
+
     </div>
 
-    <!-- Bottom Copyright -->
+    <!-- Bottom Copyright Bar -->
     <div class="footer_bottom">
         <p class="footer_copy">
-            © 2026 TangierVibes. All rights reserved.
+            &copy; <?= date('Y') ?> TangierVibes. Created with passion for Tangier.
         </p>
     </div>
 </footer>
